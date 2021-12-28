8427f07aacacb55f9ab6e063080d80da5dd06cfd
putriapriyantiwindya <putriapriyantiwindya@gmail.com>
2021-12-28 09:28:08 +0700

    OOP in Kotlin including Any Class, Type Check & Cast, toString Function, Equals Function and HashCode Function

---
+++ src/main/kotlin/com/mandiri/app/ToString.kt
@@ -0,0 +1,8 @@
+package com.mandiri.app
+
+import com.mandiri.oop.User
+
+fun main() {
+    val user = User("Putri", "secret")
+    println(user)
+}
---
+++ src/main/kotlin/com/mandiri/oop/GeneralFunction.kt
@@ -0,0 +1,5 @@
+package com.mandiri.oop
+
+class GeneralFunction {
+
+}
